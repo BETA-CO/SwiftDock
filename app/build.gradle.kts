2e5eb32e9d950d5834ed08ebfded8e5d50e52ea1
Rohit Ganji <rohitganjiapple2@gmail.com>
2026-06-30 00:24:07 +0530

    Bump versionCode to 3 and versionName to 1.1.2 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.swiftdock"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
