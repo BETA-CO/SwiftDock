c81a61b29a7d0fbddbb5b3e901407935f5361b7b
Rohit Ganji <rohitganjiapple2@gmail.com>
2026-07-01 14:34:18 +0530

    Bump version to 1.1.8 and update release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.swiftdock"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.6"
+        versionCode = 9
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
